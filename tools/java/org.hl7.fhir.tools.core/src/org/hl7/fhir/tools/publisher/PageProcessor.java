2d1277cb7473e811433996b4ee60bcdf3b3179fc
Alexandra Pavlyshina <semalex05@gmail.com>
2015-04-30 10:27:38 +0300

    Add vsnameonly to PageProcessor.java

--- tools/java/org.hl7.fhir.tools.core/src/org/hl7/fhir/tools/publisher/PageProcessor.java
+++ tools/java/org.hl7.fhir.tools.core/src/org/hl7/fhir/tools/publisher/PageProcessor.java
@@ -657,6 +657,21 @@ public class PageProcessor implements Logger, ProfileKnowledgeProvider  {
         }
       } else if (com[0].equals("toc"))
         src = s1 + generateToc() + s3;
+	  else if (com[0].equals("vsnameonly")) {
+        if (resource != null)
+          src = s1 + ((ValueSet) resource).getUrl() + s3;
+        else {
+        BindingSpecification bs = definitions.getBindingByName(Utilities.fileTitle(file));
+        if (bs == null) {
+          src = s1 + Utilities.fileTitle(file) + s3;
+			} else {
+          String reference = bs.getReference();
+          /* if (reference.startsWith("valueset-"))
+            reference = reference.substring(9); */
+          src = s1 + reference + s3;
+			}
+		  }		  
+       }
       else if (com[0].equals("txdef"))
         src = s1 + generateCodeDefinition(Utilities.fileTitle(file)) + s3;
       else if (com[0].equals("igname"))
