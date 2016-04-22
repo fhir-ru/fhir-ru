07d48b4bf50714167418cb806f3bf5365acd957e
Alexandra Pavlyshina <semalex05@gmail.com>
2016-04-22 19:12:03 +0300

    translate: index.html measure medicationorder medicationstatement messageheader namingsystem Publisher.java

--- tools/java/org.hl7.fhir.tools.core/src/org/hl7/fhir/tools/publisher/Publisher.java
+++ tools/java/org.hl7.fhir.tools.core/src/org/hl7/fhir/tools/publisher/Publisher.java
@@ -472,7 +472,7 @@ public class Publisher implements URIResolver, SectionNumberer {
         return Long.toString(revNumber);
     } catch (ClientException e) {
       System.out.println("Warning @ Unable to read the SVN version number: " + e.getMessage() );
-      return "????";
+      return "8212";
     }
 
   }
