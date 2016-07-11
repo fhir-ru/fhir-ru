0dc3b7be724b1965a074fe5070f766f9726020c9
Alexandra Pavlyshina <semalex05@gmail.com>
2016-07-11 23:36:42 +0300

    update: revision

--- tools/java/org.hl7.fhir.tools.core/src/org/hl7/fhir/tools/publisher/Publisher.java
+++ tools/java/org.hl7.fhir.tools.core/src/org/hl7/fhir/tools/publisher/Publisher.java
@@ -490,7 +490,7 @@ public class Publisher implements URIResolver, SectionNumberer {
         return Long.toString(revNumber);
     } catch (ClientException e) {
       System.out.println("Warning @ Unable to read the SVN version number: " + e.getMessage() );
-      return "8664";
+      return "8780";
     }
 
   }
