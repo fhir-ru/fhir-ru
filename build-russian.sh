da1275f5ee5945d87943a928a95aa99c70be451e
Aleksandra Pavlyshina <pavlyshina@health-samurai.io>
2021-01-10 13:22:00 -0600

    Update build to make it faster

--- build-russian.sh
+++ build-russian.sh
@@ -3,6 +3,5 @@ cd ..
 rm -rf build-fhir
 git clone --depth 1 $THISDIR build-fhir
 cp  -R $THISDIR/source.ru/* build-fhir/source
-cp  -R $THISDIR/guides.ru/* build-fhir/guides
 cd build-fhir
 ./build.sh
